35f335d83ac575333944e8115db739f95f3f5972
JDrak21 <juangonber@unisabana.edu.co>
2022-04-02 20:18:27 -0500

    Dejémoslo así por ahora. Faltan los sólidos de los muros

--- Assets/Bala.cs
+++ Assets/Bala.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bala : MonoBehaviour
 {
-  public float velocidad=2f;
+  public float velocidad=300f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +14,6 @@ public class Bala : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      transform.position += transform.forward * Time.deltaTime;
+      transform.position += transform.forward * Time.deltaTime*velocidad;
     }
 }
--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-  public float velocidad=20f;
+  public float velocidad=50f;
   public GameObject characterPrincipal;
   public GameObject bala;
+
+  private Rigidbody Físicas;
+
     // Start is called before the first frame update
     void Start()
     {
         transform.position = new Vector3 (-3.452276F, 0.1900001F, -3.704882F);
+
+        Físicas = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update () {
-        transform.position += Vector3.forward * Time.deltaTime;
         if(Input.GetKey(KeyCode.UpArrow)){
-            transform.position += transform.forward * Time.deltaTime;
+            transform.position += transform.forward * Time.deltaTime * velocidad;
         }
         if(Input.GetKey(KeyCode.DownArrow)){
-            transform.position -= transform.forward * Time.deltaTime;
+            transform.position -= transform.forward * Time.deltaTime * velocidad;
         }
         if(Input.GetKey(KeyCode.RightArrow)){
             transform.position += Vector3.right * Time.deltaTime;
@@ -31,7 +35,7 @@ public class CharacterMovement : MonoBehaviour
             transform.eulerAngles -= new Vector3(0,velocidad*Time.deltaTime,0);
         }
         if(Input.GetKeyDown(KeyCode.A)){
-            transform.position += transform.forward *velocidad * Time.deltaTime;
+            transform.position += transform.forward *velocidad * Time.deltaTime*velocidad;
         }
         if(Input.GetKey(KeyCode.Space)){
             Instantiate (bala, transform.position, transform.rotation);
